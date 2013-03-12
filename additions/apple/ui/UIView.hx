95884467c5c76b4d8b60a98cd8bbd981854f94dd
Randonee <aaron@seasonsoftware.com>
2013-03-12 16:42:24 -0700

    added UIView.animateWithDuration

--- additions/apple/ui/UIView.hx
+++ additions/apple/ui/UIView.hx
@@ -1,1 +1,8 @@
 	public static inline function UIViewDidMoveToSuperview():String{ return "UIViewDidMoveToSuperview";}
+	
+	static public function animateWithDuration(duration:Float, delay:Float, options:Int, animationsHandler:Void->Void, compleationHander:Bool->Void)
+	{
+		uiview_animateWithDuration(duration, delay, options, animationsHandler, compleationHander);
+	}
+	private static var uiview_animateWithDuration = Lib.load ("basis", "uiview_animateWithDuration", 5);
+	
