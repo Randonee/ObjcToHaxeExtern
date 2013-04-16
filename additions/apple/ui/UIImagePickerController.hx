9a1fc0e4b8de59b342f73b6f9d9c4efe39d0b2f9
Randonee <aaron@seasonsoftware.com>
2013-04-15 17:10:33 -0700

    added UIImagePickerController additions

--- additions/apple/ui/UIImagePickerController.hx
+++ additions/apple/ui/UIImagePickerController.hx
@@ -9,3 +9,9 @@
 	{
 		delegate = new UIImagePickerControllerDelegate(this);
 	}
+	
+	static function isSourceTypeAvailable(sourceType:Int):Bool
+	{
+		return uitableview_initWithFrameStyle(uiimagepickercontroller_isSourceTypeAvailable(sourceType));
+	}
+	private static var uiimagepickercontroller_isSourceTypeAvailable = Lib.load ("basis", "uiimagepickercontroller_isSourceTypeAvailable", 1);
