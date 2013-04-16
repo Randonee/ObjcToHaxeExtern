332afcc469598b59c46a77140225363645f6e6b1
Randonee <aaron@seasonsoftware.com>
2013-04-16 13:13:11 -0700

    Use correct UIIMagePickerController cffi

--- additions/apple/ui/UIImagePickerController.hx
+++ additions/apple/ui/UIImagePickerController.hx
@@ -10,8 +10,8 @@
 		delegate = new UIImagePickerControllerDelegate(this);
 	}
 	
-	static function isSourceTypeAvailable(sourceType:Int):Bool
+	public static function isSourceTypeAvailable(sourceType:Int):Bool
 	{
-		return uitableview_initWithFrameStyle(uiimagepickercontroller_isSourceTypeAvailable(sourceType));
+		return uiimagepickercontroller_isSourceTypeAvailable(sourceType);
 	}
 	private static var uiimagepickercontroller_isSourceTypeAvailable = Lib.load ("basis", "uiimagepickercontroller_isSourceTypeAvailable", 1);
