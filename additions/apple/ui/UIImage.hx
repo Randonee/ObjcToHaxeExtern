8256e5d362ecd7c1ac5ad08d8f3300dacf63b180
Randonee <aaron@seasonsoftware.com>
2013-06-24 14:05:40 -0700

    added imageFromJPEG to UIImage

--- additions/apple/ui/UIImage.hx
+++ additions/apple/ui/UIImage.hx
@@ -3,3 +3,11 @@
 		return BaseCode64.decodeBytesData(uiimage_imageToBase64JPEG(image.basisID, quality));
 	}
 	private static var uiimage_imageToBase64JPEG = Lib.load ("basis", "uiimage_imageToBase64JPEG", 2);
+	
+	static public function imageFromJPEG(bytes:Bytes):UIImage
+	{
+		var data:String = BaseCode64.enocdeBytesData(bytes.getData());
+		var imageID:String = uiimage_imageFromBase64JPEG(data, data.length);
+		return cast(BasisApplication.instance.objectManager.getObject(imageID), UIImage);
+	}
+	private static var uiimage_imageFromBase64JPEG = Lib.load ("basis", "uiimage_imageFromBase64JPEG", 2);
