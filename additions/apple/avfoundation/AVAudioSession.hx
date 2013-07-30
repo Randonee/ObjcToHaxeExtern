d36cc009aa950d5ad0d6184cff838a0a223e5578
Randonee <aaron@seasonsoftware.com>
2013-07-30 11:28:51 -0700

    added AVAudioRecorder additions

---
+++ additions/apple/avfoundation/AVAudioSession.hx
@@ -0,0 +1,21 @@
+	static public function AVAudioSessionCategoryAmbient():String{return avaudiosession_AVAudioSessionCategoryAmbient();}
+	private static var avaudiosession_AVAudioSessionCategoryAmbient = Lib.load ("basis", "avaudiosession_AVAudioSessionCategoryAmbient", 0);
+	
+	static public function AVAudioSessionCategorySoloAmbient():String{return avaudiosession_AVAudioSessionCategorySoloAmbient();}
+	private static var avaudiosession_AVAudioSessionCategorySoloAmbient = Lib.load ("basis", "avaudiosession_AVAudioSessionCategorySoloAmbient", 0);
+	
+	static public function AVAudioSessionCategoryPlayback():String{return avaudiosession_AVAudioSessionCategoryPlayback();}
+	private static var avaudiosession_AVAudioSessionCategoryPlayback = Lib.load ("basis", "avaudiosession_AVAudioSessionCategoryPlayback", 0);
+	
+	static public function AVAudioSessionCategoryRecord():String{return avaudiosession_AVAudioSessionCategoryRecord();}
+	private static var avaudiosession_AVAudioSessionCategoryRecord = Lib.load ("basis", "avaudiosession_AVAudioSessionCategoryRecord", 0);
+	
+	static public function AVAudioSessionCategoryPlayAndRecord():String{return avaudiosession_AVAudioSessionCategoryPlayAndRecord();}
+	private static var avaudiosession_AVAudioSessionCategoryPlayAndRecord = Lib.load ("basis", "avaudiosession_AVAudioSessionCategoryPlayAndRecord", 0);
+	
+	static public function AVAudioSessionCategoryAudioProcessing():String{return avaudiosession_AVAudioSessionCategoryAudioProcessing();}
+	private static var avaudiosession_AVAudioSessionCategoryAudioProcessing = Lib.load ("basis", "avaudiosession_AVAudioSessionCategoryAudioProcessing", 0);
+	
+	static public function AVAudioSessionCategoryMultiRoute():String{return avaudiosession_AVAudioSessionCategoryMultiRoute();}
+	private static var avaudiosession_AVAudioSessionCategoryMultiRoute = Lib.load ("basis", "avaudiosession_AVAudioSessionCategoryMultiRoute", 0);
+	
