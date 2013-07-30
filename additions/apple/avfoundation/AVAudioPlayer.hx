f92cc97bd1d97151a0b7d81ad4c32ec8782595ff
Randonee <aaron@seasonsoftware.com>
2013-07-30 15:32:18 -0700

    Add AVAudioPlayer methods

--- additions/apple/avfoundation/AVAudioPlayer.hx
+++ additions/apple/avfoundation/AVAudioPlayer.hx
@@ -1,3 +1,14 @@
+	static public function initWithContentsOfURLError(url:String):AVAudioPlayer
+	{
+		var objectID:String = avaudioplayer_initWithContentsOfURLError(url);
+		var object:IObject = BasisApplication.instance.objectManager.getObject(objectID);
+		if(object != null)
+			return cast(object, AVAudioPlayer);
+
+		return null;
+	}
+	private static var avaudioplayer_initWithContentsOfURLError = Lib.load ("basis", "avaudioplayer_initWithContentsOfURLError", 1);
+
 	public var delegate(default, null):AVAudioPlayerDelegate;
 	
 	public function new(?type:Class<IObject>=null)
